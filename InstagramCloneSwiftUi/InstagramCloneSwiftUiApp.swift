5d741c04c3a424c86a9aaed90d20c699bc88dabd
Tornike <tornikeeristavi43@gmail.com>
2025-02-05 11:56:08 +0400

    Initial Commit

---
+++ InstagramCloneSwiftUi/InstagramCloneSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSwiftUiApp.swift
+//  InstagramCloneSwiftUi
+//
+//  Created by Tornike Eristavi on 05.02.25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
